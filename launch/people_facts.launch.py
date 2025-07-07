ff99e194925cdefda5022e126de84fb12bfac740
Noel Jimenez <noel.jimenez@pal-robotics.com>
2025-07-07 12:34:58 +0200

    Add diagnostic analyzers

--- launch/people_facts.launch.py
+++ launch/people_facts.launch.py
@@ -12,16 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from ament_index_python import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import EmitEvent, RegisterEventHandler
 from launch.events import matches_action
-from launch_ros.actions import LifecycleNode
+from launch_ros.actions import LifecycleNode, Node
 from launch_ros.events.lifecycle import ChangeState
 from launch_ros.event_handlers import OnStateTransition
 from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
+    pkg = 'people_facts'
+
     people_facts_node = LifecycleNode(
         package='people_facts', executable='people_facts', namespace='',
         name='people_facts')
@@ -36,7 +42,19 @@ def generate_launch_description():
             lifecycle_node_matcher=matches_action(people_facts_node),
             transition_id=Transition.TRANSITION_ACTIVATE))]))
 
+    people_facts_analyzer = Node(
+        package='diagnostic_aggregator',
+        executable='add_analyzer',
+        namespace=pkg,
+        output='screen',
+        emulate_tty=True,
+        parameters=[
+            os.path.join(get_package_share_directory(pkg), 'config', f'{pkg}_analyzers.yaml')],
+    )
+
     return LaunchDescription([
         people_facts_node,
         configure_event,
-        activate_event])
+        activate_event,
+        people_facts_analyzer
+    ])
